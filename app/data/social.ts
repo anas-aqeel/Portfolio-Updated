7b179cf8d336f2c39bb995d661f3cee41d6b1c38
haris-aqeel <harisaqeel.2001@gmail.com>
2023-09-22 20:19:37 +0500

    Project Updated

---
+++ app/data/social.ts
@@ -0,0 +1,97 @@
+import {
+  BiLinkExternal,
+  BiLogoCodepen,
+  BiLogoDribbble,
+  BiLogoGithub,
+  BiLogoGitlab,
+  BiLogoLinkedinSquare,
+  BiLogoTwitter,
+  BiLogoUnsplash,
+  BiLogoYoutube,
+} from "react-icons/bi";
+import { FaFreeCodeCamp } from "react-icons/fa";
+import { FaHashnode } from "react-icons/fa6";
+
+export const socialLinks = [
+  {
+    id: 1,
+    name: "GitHub",
+    url: "https://github.com/anas-aqeel",
+    icon: BiLogoGithub,
+    status: "social",
+  },
+  {
+    id: 2,
+    name: "Twitter",
+    url: "https://twitter.com/victorekea",
+    icon: BiLogoTwitter,
+    status: "social",
+  },
+  {
+    id: 3,
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/anas452111/",
+    icon: BiLogoLinkedinSquare,
+    status: "social",
+  },
+  {
+    id: 4,
+    name: "Codepen",
+    url: "https://codepen.io/anasaqeel",
+    icon: BiLogoCodepen,
+    status: "social",
+  },
+  {
+    id: 5,
+    name: "Dribbble",
+    url: "https://dribbble.com/Aqeel152",
+    icon: BiLogoDribbble,
+    status: "social",
+  },
+
+
+  {
+    id: 8,
+    name: "Unsplash",
+    url: "https://unsplash.com/@anas_aqeel",
+    icon: BiLogoUnsplash,
+    status: "social",
+  },
+  {
+    id: 9,
+    name: "FreeCodeCamp",
+    url: "https://www.freecodecamp.org/Anas_Aqeel",
+    icon: FaFreeCodeCamp,
+    status: "publication",
+  },
+  {
+    id: 10,
+    name: "Hashnode",
+    url: "https://frontend-guide.hashnode.dev/",
+    icon: FaHashnode,
+    status: "publication",
+  },
+  {
+    id: 12,
+    name: "Youtube",
+    url: "https://www.youtube.com/@AnasAqeel-be8ef/featured",
+    icon: BiLogoYoutube,
+    status: "social",
+  },
+  {
+    id: 13,
+    name: "Daily.dev",
+    url: "https://app.daily.dev/anas56",
+    icon: BiLinkExternal,
+    status: "social",
+  },
+
+
+  {
+    id: 17,
+    name: "Gitlab",
+    url: "https://gitlab.com/anasaqeel.4444",
+    icon: BiLogoGitlab,
+    status: "social",
+  },
+];
